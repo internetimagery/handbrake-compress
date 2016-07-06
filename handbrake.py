0ad05e04a7b30fde99bcd4a8a2ceadb1df897b30
Jason Dixon <jason.dixon.email@gmail.com>
2016-07-06 23:47:30 +1200

    compression preset by default

--- handbrake.py
+++ handbrake.py
@@ -9,14 +9,16 @@ import platform
 # Find handbrakes executable based on OS
 OS = platform.system()
 if OS == "Windows":
-    handbrake_path = "C:\\Program Files\\Handbrake\\HandBrakeCLI.exe"
+    HANDBRAKE_PATH = "C:\\Program Files\\Handbrake\\HandBrakeCLI.exe"
 else:
     raise(Exception("Cannot find Handbrake executable."))
-if not os.path.isfile(handbrake_path):
-    raise(Exception("Cannot find Handbrake at %s" % handbrake_path))
+if not os.path.isfile(HANDBRAKE_PATH):
+    raise(Exception("Cannot find Handbrake at %s" % HANDBRAKE_PATH))
+
+EXTENSION = ".m4v" #".mkv"
 
 def compress(video_path, options = None):
-    compressed_path = "%s.mkv" % os.path.splitext(video_path)[0]
+    compressed_path = "%s.%s" % (os.path.splitext(video_path)[0], EXTENSION)
     # Check we are ok to make the compression
     if not os.path.isfile(video_path):
         raise(Exception("File cannot be found: %s" % video_path))
@@ -24,8 +26,7 @@ def compress(video_path, options = None):
         raise(Exception("Compressed file already exists: %s" % compressed_path))
     # Compress the file
     try:
-        print("COMPRESSING: %s" % video_path)
-        args = [handbrake_path,
+        args = [HANDBRAKE_PATH,
             "--preset", "High Profile"] #"Universal",
         if type(options) == list:
             args.extend(options)
@@ -37,6 +38,7 @@ def compress(video_path, options = None):
         if os.path.isfile(compressed_path):
             os.unlink(compressed_path)
         raise
+    return compressed_path
 
 if __name__ == '__main__':
     # Test out a video
@@ -46,5 +48,5 @@ if __name__ == '__main__':
         os.mkdir(test)
     test_video = os.path.join(test, "video.mov")
     if not os.path.isfile(test_video):
-        raise(Exception("No test video found"))
+        raise(Exception("No test video found. Please add one named \"video.mov\""))
     compress(test_video)
